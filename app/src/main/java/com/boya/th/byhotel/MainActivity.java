d4bea8a905e37c08a2d0e472fae600f36d94eeaf
cuijuntao <610413008@qq.com>
2018-11-20 17:38:40 +0800

    1.2次提交

--- app/src/main/java/com/boya/th/byhotel/MainActivity.java
+++ app/src/main/java/com/boya/th/byhotel/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String cui;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cui = "test";
     }
 }
